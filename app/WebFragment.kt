470fdc24e10c1d618a39f896b9e5664e050654ea
carolinedeppe <rcdeppe@gmail.com>
2023-02-24 18:20:38 +0000

    android up to page 290

---
+++ app/WebFragment.kt
@@ -0,0 +1,5 @@
+import dev.hotwire.turbo.fragments.TurboWebFragment
+import dev.hotwire.turbo.nav.TurboNavGraphDestination
+
+@TurboNavGraphDestination(uri = "piazza://fragment/web")
+open class WebFragment : TurboWebFragment() {}
